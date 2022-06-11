23efaa35e475012cf1ed28e13fba18a1970f9f14
CRON <fascalsj@gmail.com>
2022-06-11 22:19:59 +0700

    ECM-1 Inititate Project

---
+++ src/main/java/com/github/portforesearch/ecommurzbe/EcommurzBeApplication.java
@@ -0,0 +1,13 @@
+package com.github.portforesearch.ecommurzbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommurzBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommurzBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/portforesearch/ecommurzbe/EcommurzBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.portforesearch.ecommurzbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommurzBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
